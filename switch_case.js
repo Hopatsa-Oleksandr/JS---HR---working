49c978cef74a20cbaebe9da84421973c5f201af0
Hopatsa-Oleksandr <bc.alexg@gmail.com>
2021-12-07 15:46:38 +0200

    Добавляет практику

---
+++ switch_case.js
@@ -0,0 +1,19 @@
+switch (Alex) {
+  case 1:
+    console.log("Вы плохо подготовились!");
+    break;
+  case 2:
+    console.log("Неудовлетворительно!");
+    break;
+  case 3:
+    console.log("Можно было и лучше!");
+    break;
+  case 4:
+    console.log("Хорошо, постарался!");
+    break;
+  case 5:
+    console.log("Молодец! Отлично!");
+    break;
+  default:
+    console.log("Такой оценки не существует!");
+}
